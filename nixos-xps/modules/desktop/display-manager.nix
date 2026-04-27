710fa7e23e24a780561813ace233c82ed9b0bc41
Jose Villamizar <57684713+mcvills@users.noreply.github.com>
2026-04-27 16:53:40 -0400

    Delete nixos-xps directory

--- nixos-xps/modules/desktop/display-manager.nix
+++
@@ -1,8 +0,0 @@
-# Display manager settings shared between Hyprland and GNOME
-{...}: {
-  # GDM is used as fallback when switching to GNOME session
-  services.xserver.displayManager.gdm = {
-    enable = false;   # tuigreet is primary; flip to true for GNOME-only mode
-    wayland = true;
-  };
-}
--- nixos-xps/modules/hardware/bluetooth.nix
+++
@@ -1,8 +0,0 @@
-{pkgs, ...}: {
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings.Policy.AutoEnable = "true";
-  };
-  services.blueman.enable = true;
-}
